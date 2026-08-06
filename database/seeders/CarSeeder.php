98e3758a2ece72eb0a87e3fc9e52eaf469599593
Cristina <cristina.coman.bar@gmail.com>
2026-08-06 16:50:16 -0400

    Make car seeder fully reproducible

--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -1,55 +1,80 @@
 <?php
 
 namespace Database\Seeders;
-use App\Models\Car;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Car;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class CarSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-   public function run(): void
-{
-    Car::create([
-        'brand' => 'Toyota',
-        'model' => 'RAV4',
-        'year' => 2024,
-        'price_per_day' => 89.99,
-        'available' => true,
-        'fuel_type' => 'Gasoline',
-        'transmission' => 'Automatic',
-        'seats' => 5,
-        'category' => 'SUV',
-        'photo' => 'cars/toyota-rav4.jpg',
-    ]);
+    public function run(): void
+    {
+        $sourceFolder = database_path('seeders/data/cars');
+        $destinationFolder = storage_path('app/public/cars');
 
-    Car::create([
-        'brand' => 'Mini',
-        'model' => 'Cooper',
-        'year' => 2023,
-        'price_per_day' => 69.99,
-        'available' => true,
-        'fuel_type' => 'Gasoline',
-        'transmission' => 'Automatic',
-        'seats' => 4,
-        'category' => 'Hatchback',
-        'photo' => 'cars/mini-cooper.jpg',
-    ]);
+        File::ensureDirectoryExists($destinationFolder);
 
-    Car::create([
-        'brand' => 'Ford',
-        'model' => 'Mustang',
-        'year' => 2024,
-        'price_per_day' => 129.99,
-        'available' => true,
-        'fuel_type' => 'Gasoline',
-        'transmission' => 'Automatic',
-        'seats' => 4,
-        'category' => 'Sports',
-        'photo' => 'cars/mustang.jpg',
-    ]);
-}
+        $cars = [
+            [
+                'brand' => 'Toyota',
+                'model' => 'RAV4',
+                'year' => 2024,
+                'price_per_day' => 89.99,
+                'available' => true,
+                'fuel_type' => 'Hybrid',
+                'transmission' => 'Automatic',
+                'seats' => 5,
+                'category' => 'SUV',
+                'photo' => 'cars/toyota-rav4.png',
+                'image' => 'toyota-rav4.png',
+            ],
+            [
+                'brand' => 'Mini',
+                'model' => 'Cooper',
+                'year' => 2023,
+                'price_per_day' => 59.99,
+                'available' => true,
+                'fuel_type' => 'Gasoline',
+                'transmission' => 'Automatic',
+                'seats' => 4,
+                'category' => 'Compact',
+                'photo' => 'cars/mini-cooper.png',
+                'image' => 'mini-cooper.png',
+            ],
+            [
+                'brand' => 'Dodge',
+                'model' => 'Caravan',
+                'year' => 2022,
+                'price_per_day' => 99.99,
+                'available' => true,
+                'fuel_type' => 'Gasoline',
+                'transmission' => 'Automatic',
+                'seats' => 7,
+                'category' => 'Van',
+                'photo' => 'cars/dodge-caravan.png',
+                'image' => 'dodge-caravan.png',
+            ],
+        ];
+
+        foreach ($cars as $car) {
+            File::copy(
+                $sourceFolder . DIRECTORY_SEPARATOR . $car['image'],
+                $destinationFolder . DIRECTORY_SEPARATOR . $car['image']
+            );
+
+            unset($car['image']);
+
+            Car::updateOrCreate(
+                [
+                    'brand' => $car['brand'],
+                    'model' => $car['model'],
+                ],
+                $car
+            );
+        }
+    }
 }
